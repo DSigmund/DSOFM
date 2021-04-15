116c264b5ba06df58a50ef00580ca2450e6ab2e4
Dominik Sigmund <dominik.sigmund@br.de>
2021-04-15 13:22:32 +0200

    Added Fonts

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ do
 	insert+=$(printf ".CHAPTER \"${i}\",")
 done
 insert=$(echo "$insert" | sed 's/.ms//g')
-sed -e "s/%CONTENT%/${insert}/g" index.ms | tr , "\n" | soelim | ghighlight.pl | groff -ms -dpaper=a5 -P-pa5 -T pdf > dist/dsofm_raw.pdf
+sed -e "s/%CONTENT%/${insert}/g" index.ms | tr , "\n" | soelim | groff -ms -dpaper=a5 -P-pa5 -T pdf > dist/dsofm_raw.pdf
 gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=dist/dsofm.pdf assets/dsofm_2020_V2_a5.pdf dist/dsofm_raw.pdf
 rm dist/dsofm_raw.pdf
